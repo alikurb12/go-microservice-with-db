7d95dd25f7c65f83c2484ae8360e838dc2f1b55f
alikurb12 <gurbansahedovali@gmail.com>
2026-04-06 12:51:27 +0300

    create request id middleware

---
+++ internal/protocol/httpapi/middleware.go
@@ -0,0 +1,18 @@
+package httpapi
+
+import (
+	"net/http"
+
+	"github.com/alikurb12/go-microservice-with-db/internal/domain"
+)
+
+func RequestIDMiddleware(genID domain.IDGenerator) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			requestID := genID.NewID()
+			w.Header().Set(RequestIdHeader, requestID)
+			next.ServeHTTP(w, r)
+		})
+	}
+
+}
