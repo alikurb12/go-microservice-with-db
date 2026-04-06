b12726971406ae7a521a36497f5af4d5e2d48763
alikurb12 <gurbansahedovali@gmail.com>
2026-04-06 14:50:45 +0300

    new uuid 7 implementation

--- internal/infrastructure/uuid_generator.go
+++ internal/infrastructure/uuid_generator.go
@@ -5,5 +5,6 @@ import "github.com/google/uuid"
 type UUIDGenerator struct{}
 
 func (UUIDGenerator) NewID() string {
-	return uuid.NewString()
+	// return uuid.NewString()
+	return uuid.Must(uuid.NewV7()).String()
 }
