ca629f846753a58f39a867e4154055373e501c4d
alikurb12 <gurbansahedovali@gmail.com>
2026-04-06 15:15:48 +0300

    use sqlite clock for dt handler

--- cmd/server/http_handler.go
+++ cmd/server/http_handler.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/alikurb12/go-microservice-with-db/internal/domain"
 	"github.com/alikurb12/go-microservice-with-db/internal/infrastructure"
+	"github.com/alikurb12/go-microservice-with-db/internal/infrastructure/sqlite_clock"
 	"github.com/alikurb12/go-microservice-with-db/internal/protocol/httpapi"
 )
 
 func buildHTTPHandler() http.Handler {
 	idGen := infrastructure.UUIDGenerator{}
-	clock := infrastructure.SystemClock{}
-	systemClockService := domain.NewDateTimeService(clock)
+	// clock := infrastructure.SystemClock{}
+	// systemClockService := domain.NewDateTimeService(clock)
+
+	db, err := sqlite_clock.Open("app.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sqlClock := sqlite_clock.NewSQLiteClock(db)
+	sqliteClockService := domain.NewDateTimeService(sqlClock)
 
 	mux := http.NewServeMux()
-	mux.Handle("GET /", httpapi.CreateDateTimeHandler(systemClockService))
+	mux.Handle("GET /", httpapi.CreateDateTimeHandler(sqliteClockService))
 	
 	var handler http.Handler = mux
 	handler = httpapi.RequestIDMiddleware(idGen)(handler)
