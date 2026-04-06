be48348e5e2d574d20cf4b81d0c6e36fbfec5ab0
alikurb12 <gurbansahedovali@gmail.com>
2026-04-06 13:43:13 +0300

    register datetime handler

--- cmd/server/http_handler.go
+++ cmd/server/http_handler.go
@@ -3,13 +3,18 @@ package main
 import (
 	"net/http"
 
+	"github.com/alikurb12/go-microservice-with-db/internal/domain"
 	"github.com/alikurb12/go-microservice-with-db/internal/infrastructure"
 	"github.com/alikurb12/go-microservice-with-db/internal/protocol/httpapi"
 )
 
 func buildHTTPHandler() http.Handler {
 	idGen := infrastructure.UUIDGenerator{}
+	clock := infrastructure.SystemClock{}
+	systemClockService := domain.NewDateTimeService(clock)
+
 	mux := http.NewServeMux()
+	mux.Handle("GET /", httpapi.CreateDateTimeHandler(systemClockService))
 	
 	var handler http.Handler = mux
 	handler = httpapi.RequestIDMiddleware(idGen)(handler)
